d51665f39980c96b1a0d7ab51955b50783074fb9
Ali Mert Özhayta <ozhayta@zoho.com>
2019-12-25 20:21:04 +0300

    Initial Commit

---
+++ Movie DB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie DB
+//
+//  Created by Ali Mert Özhayta on 25.12.2019.
+//  Copyright © 2019 Ali Mert Özhayta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
